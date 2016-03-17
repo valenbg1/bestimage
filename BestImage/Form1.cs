f7a2ec591f004688f8c15c11eeaad53fe83f5f76
Valen <valentin_gomez1@hotmail.com>
2016-03-17 17:26:57 +0100

    Changes to interface and added directory search

--- BestImage/Form1.cs
+++ BestImage/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace BestImage
 {
     public partial class Form1 : Form
     {
+        private FolderBrowserDialog folderBrowserDialog1;
+
         public Form1()
         {
             InitializeComponent();
+            this.folderBrowserDialog1 = new FolderBrowserDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
         }
     }
 }
